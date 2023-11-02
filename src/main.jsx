cd9d9836e7edabefcca42ccac46acfcf0b4d3b74
vivek <guptavivek1035@gmail.com>
2023-11-02 22:59:41 +0530

    gfgf

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from "./Layouts/Layout.jsx"
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout/>}>
+            <Route path="" element>
+        </Route  >
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
